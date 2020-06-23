1447dd7da1e7318a782fef962448034b4795e4d2
yaotani <a.yaotani0413@gmail.com>
2020-06-23 23:06:35 +0900

    remove extra code

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,16 +21,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.header__title {
-  border-bottom: 2px solid #ffcd28;
-  margin: 0;
-  width: 100%;
-  text-align: center;
-  line-height: 100px;
-  color: #ffcd28;
-  font-size: 40px;
-  font-weight: bold;
-}
 
 .header-padding {
   padding-top: 100px;
@@ -39,14 +29,6 @@ export default {
 .main {
   display: flex;
   flex-direction: column;
-}
-.header {
-  z-index: 10; /* todo: 必要に応じて変更 */
-  position: fixed;
-  top: 0px;
-  width: 100vw;
-  height: 100px;
-  background-color: #242424;
 }
 
 // .footer {
